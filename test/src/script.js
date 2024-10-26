b1db79fa0d9b2e8afd86fdecfac85456acbe593b
beta160900 <beta010js@gmail.com>
2024-10-26 12:56:26 +0700

    cm2

---
+++ test/src/script.js
@@ -0,0 +1,11 @@
+fetch("nav.html")
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById("nav-placeholder").innerHTML = data;
+            });
+
+fetch("footer.html")
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById("footer-placeholder").innerHTML = data;
+            });
